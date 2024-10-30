177943eb7430f7bc6a8a9707b22afcbc443894c4
Sanne 't Hooft <shooft@me.com>
2024-10-30 14:57:15 +0100

    tools, responsive en performant

--- scripts/landmark--header.js
+++ scripts/landmark--header.js
@@ -1,23 +1,286 @@
 const menu = document.querySelector('menu');
 const buttons = menu.querySelectorAll('button');
+const theThing = document.querySelector('.thé-thing');
 
-function handleButtonClick(e) {
-	// remove current pressed
-	const currentButton = menu.querySelector('[aria-pressed=true]');
-	if (currentButton) {
-		currentButton.ariaPressed = false;
+
+
+
+
+////////////
+// ESCAPE //
+////////////
+
+document.onkeydown = handleKeyDown;
+
+function handleKeyDown(e) {
+	if (e.key == "Escape") {
+		const currentButton = document.querySelector('[aria-pressed="true"]');
+		if (currentButton) {
+			const currentMode = currentButton.value;
+
+			switchButtons(currentButton, false, currentMode, false);
+			switchModes(currentMode, false);
+		}
 	}
-	
-	// add pressed
-	const button = e.currentTarget;
-	if (currentButton == button ) {
-		document.documentElement.dataset.mode = null;
-	} else {
-		button.ariaPressed = true;
-		document.documentElement.dataset.mode = button.value;
-	}
-}
+};
+
+
+
+
+
+/////////////
+// KLIKKEN //
+/////////////
 
 buttons.forEach(button => {
 	button.onclick = handleButtonClick;
 });
+
+
+function handleButtonClick(e) {
+	const newButton = e.currentTarget;
+	const currentButton = menu.querySelector('[aria-pressed=true]');
+	
+	// determine current & new mode //
+
+	let currentMode, newMode;
+
+	if ( currentButton ) {
+		currentMode = currentButton.value;
+	} else {
+		currentMode = false;
+	}
+
+	if ( currentButton == newButton ) {
+		newMode = false;
+	} else {
+		newMode = newButton.value;
+	}
+
+	switchButtons(currentButton, newButton, currentMode, newMode);
+	switchModes(currentMode, newMode);
+}
+
+
+
+
+
+////////////////////
+// SWITCH BUTTONS //
+////////////////////
+
+function switchButtons(currentButton, newButton, currentMode, newMode) {
+
+	// add and/or remove pressed //
+
+	if ( newMode == false ) {
+		currentButton.ariaPressed = false;
+	} else {
+		newButton.ariaPressed = true;
+		if ( currentMode != false ) {
+			currentButton.ariaPressed = false;
+		}
+	}
+}
+
+
+
+
+
+//////////////////
+// SWITCH MODES //
+//////////////////
+
+function switchModes(currentMode, newMode) {
+
+	closeCurrentMode();
+
+
+
+  ////////////////////////
+	// CLOSE CURRENT MODE //
+	////////////////////////
+
+	function closeCurrentMode() {
+
+		if (currentMode) {
+
+			////////////
+			// always //
+			////////////
+
+			closeInfo();
+			document.documentElement.dataset.modeStatus = false;
+
+			/////////////////
+			// close tools //
+			/////////////////
+			if (currentMode == "tools") {
+				disableButtons();
+				setTimeout(() => {
+					document.documentElement.dataset.mode = false;
+					openNewMode();
+				}, 1350);
+			}
+
+			//////////////////////
+			// close accessible //
+			//////////////////////
+			else if (currentMode == "accessible") {
+				// disableButtons();
+
+				setTimeout(() => {
+					document.documentElement.dataset.mode = false;
+					openNewMode();
+				}, 200);
+			}
+			
+			//////////////////////
+			// close responsive //
+			//////////////////////
+			else if (currentMode == "responsive") {
+				
+
+				if (theThing.hasAttribute("style")) {
+					theThing.removeAttribute("style");
+					disableButtons();
+
+					setTimeout(() => {
+						document.documentElement.dataset.mode = false;
+						openNewMode();
+					}, 1300);
+				}
+				
+				else {
+					setTimeout(() => {
+						document.documentElement.dataset.mode = false;
+						openNewMode();
+					}, 200);
+				}
+
+			}
+
+			//////////////////////
+			// close performant //
+			//////////////////////
+			else if (currentMode == "performant") {
+				disableButtons();
+
+				setTimeout(() => {
+					document.documentElement.dataset.mode = false;
+					openNewMode();
+				}, 1300);
+			}
+			
+		}
+
+		else {
+			openNewMode();
+		}
+
+	}
+
+
+
+	///////////////////
+	// OPEN NEW MODE //
+	///////////////////
+
+	function openNewMode() {
+		if (newMode) {
+
+			////////////
+			// always //
+			////////////
+			document.documentElement.dataset.mode = newMode;
+
+			setTimeout(() => {
+				document.documentElement.dataset.modeStatus = true;
+			
+				////////////////
+				// open tools //
+				////////////////
+				if (newMode == "tools") {
+					disableButtons();
+
+					setTimeout(() => {
+						enableButtons();
+					}, 2100);
+				}
+
+				/////////////////////
+				// open accessible //
+				/////////////////////
+				if (newMode == "accessible") {
+					disableButtons();
+
+					setTimeout(() => {
+						enableButtons();
+					}, 0);
+				}
+				
+				/////////////////////
+				// open responsive //
+				/////////////////////
+				else if (newMode == "responsive") {
+					setTimeout(() => {
+						enableButtons();
+					}, 0);
+				}
+				
+				/////////////////////
+				// open performant //
+				/////////////////////
+				if (newMode == "performant") {
+					disableButtons();
+
+					setTimeout(() => {
+						enableButtons();
+					}, 2100);
+				}
+
+			}, 50);
+		}
+
+		else {
+			enableButtons();
+		}
+	}
+
+
+
+	//////////////////////////////////
+	// CLOSE INFO / DETAILS/SUMMARY //
+	//////////////////////////////////
+
+	function closeInfo() {
+		const openInfo = document.querySelector(".info[open]");
+		if (openInfo) {
+			openInfo.open = false;
+		}
+
+	}
+
+
+
+	////////////////////
+	// DISABLE BUTTON //
+	////////////////////
+
+	function disableButtons() {
+		buttons.forEach(button => {
+			button.disabled = true;
+		});
+	}
+
+
+	///////////////////
+	// ENABLE BUTTON //
+	///////////////////
+
+	function enableButtons() {
+		buttons.forEach(button => {
+			button.disabled = false;
+		});
+	}
+}
--- scripts/landmark--main__website.js
+++ scripts/landmark--main__website.js
@@ -1,8 +1,37 @@
-const websiteMenuButton = document.querySelector(".website header button");
-const websiteHeader = document.querySelector(".website header");
+// MENU BUTTON
 
-function handleWebsiteMenuButtonClick() {
-	websiteHeader.classList.toggle("is-open");
-}
+const websiteHeader = document.querySelector(".website header");
+const websiteMenuButton = websiteHeader.querySelector("button");
+
 
 websiteMenuButton.onclick = handleWebsiteMenuButtonClick;
+
+function handleWebsiteMenuButtonClick() {
+	websiteHeader.classList.toggle("is--open");
+}
+
+
+
+
+
+// HEADER LINKS
+
+const website = document.querySelector(".website");
+const websiteHeaderLinks = website.querySelectorAll("header a");
+
+websiteHeaderLinks.forEach(websiteHeaderLink => {
+	websiteHeaderLink.onclick = handleWebsiteHeaderLinkClick;
+});
+
+
+function handleWebsiteHeaderLinkClick(e) {
+	e.preventDefault();
+
+	const targetID = e.target.getAttribute("href");
+	const targetElement = document.querySelector(targetID);
+	const targetElementTop = targetElement.getBoundingClientRect().top;
+	const websiteTop = website.getBoundingClientRect().top;
+	const fontSize =  parseFloat( window.getComputedStyle(website).getPropertyValue('font-size') );
+
+	website.scrollTop =  website.scrollTop + targetElementTop - (8.75 * fontSize) - websiteTop;
+}
